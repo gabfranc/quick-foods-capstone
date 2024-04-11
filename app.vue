3ccbb66505a3f825d7e9154590d421ef3ffccebc
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-04-11 08:27:54 -0600

    ojojojo

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NavBar />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+  <LazyFooter />
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,ts,vue}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts,vue}",
+    "./app.vue",
+  ],
+  content: [],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    colors: {
+      'white': '#e8e5da',
+      'black': '#080705',
+      'yellow': '#cdc392',
+      'orange': '#ffd670',
+      'maroon': '#912f40'
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  }
+};
